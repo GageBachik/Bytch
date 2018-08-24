4db1831ceb3211999fc9110daa339300065bc32c
Gage Bachik <gbachik>
2018-08-23 23:58:26 -0700

    init files

---
+++ ios/Pods/Target Support Files/Pods-Bytch-tvOSTests/Pods-Bytch-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Bytch_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_Bytch_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-BytchTests/Pods-BytchTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BytchTests : NSObject
+@end
+@implementation PodsDummy_Pods_BytchTests
+@end
